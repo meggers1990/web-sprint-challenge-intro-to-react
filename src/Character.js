61b916f31ec35ac293b37983219a237172c50e36
megan huckabay <huckabay90@gmail.com>
2023-07-14 03:32:42 -0700

    fixed errors

--- src/Character.js
+++ src/Character.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const CharacterContainer = styled.div`
+  margin-bottom: 20px;
+  padding: 10px;
+  border: 1px solid #ccc;
+`;
+
+const CharacterName = styled.h2`
+  margin-bottom: 5px;
+`;
+
+const CharacterDetails = styled.div`
+  font-size: 14px;
+`;
+
 const Character = ({ character }) => {
   return (
-    <div>
-      <h2>{character.name}</h2>
-      <p>Height: {character.height}</p>
-      <p>Mass: {character.mass}</p>
-      <p>Gender: {character.gender}</p>
-    </div>
+    <CharacterContainer>
+      <CharacterName>{character.name}</CharacterName>
+      <CharacterDetails>
+        <p>Height: {character.height}</p>
+        <p>Mass: {character.mass}</p>
+        <p>Hair Color: {character.hair_color}</p>
+        <p>Skin Color: {character.skin_color}</p>
+        <p>Eye Color: {character.eye_color}</p>
+        <p>Birth Year: {character.birth_year}</p>
+        <p>Gender: {character.gender}</p>
+      </CharacterDetails>
+    </CharacterContainer>
   );
 };
-
-<CharacterName>{Character.name}</CharacterName>
-
-const CharacterName = styled.div`
-  color: #333;
-  font-weight: bold;
-  margin-bottom: 10px;
-`;
 
 export default Character;
