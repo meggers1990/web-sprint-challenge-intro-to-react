47a681f172bc6fbde0e9566c59d2c41887032267
megan huckabay <huckabay90@gmail.com>
2023-07-15 01:13:47 -0700

    getting throught it

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Character from './Character';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  text-align: center;
+  align-items: center;
+  justify-content: space-around;
+  width: 70%;
+  margin: 0 auto;
+`
 
 const App = () => {
-  const [characters, setCharacters] = useState([]);
+// Try to think through what state you'll need for this app before starting. Then build out
+// the state properties here.
+const [data, setData] = useState([])
+// Fetch characters from the API in an effect hook. Remember, anytime you have a 
+// side effect in a component, you want to think about which state and/or props it should
+// sync up with, if any.
 
-  useEffect(() => {
-    axios
-      .get('https://swapi.dev/api/people/')
-      .then((response) => {
-        setCharacters(response.data.results);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+useEffect(() => {
+    axios.get("https://swapi.py4e.com/api/people/")
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-      {characters.map((character) => (
-        <Character key={character.name} character={character} />
-      ))}
-    </div>
-  );
-};
+    .then((res)=>{
+      console.log(res)
+      setData(res.data.results)
+
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+
+}, [])
+
+return (
+  <div className="App">
+  <h1 className="Header">Star Wars Characters</h1>
+  <StyledApp>
+
+      <Character data = {data} />
+      </StyledApp>
+  </div>
+);
+}
 
 export default App;
